bda5902e60b146c4a72c743b45a9efbc660071c5
Slach <slach@lingualeo.com>
2013-04-23 21:23:31 +0600

    prepare single example for mplayer osx

--- wx.expreriments/MplayerCtrlMacOSX.py
+++ wx.expreriments/MplayerCtrlMacOSX.py
@@ -6,9 +6,10 @@ import wx
 import sys
 
 class Frame(wx.Frame):
-    def __init__(self, parent, id, files):
-        wx.Frame.__init__(self, parent, id, size=(-1, -1))
+    def __init__(self, parent, frame_id, files):
+        wx.Frame.__init__(self, parent, frame_id, size=(-1, -1))
         self.panel = wx.Panel(self)
+        self.files = files
 
         mplayer_args=[
            u'--no-autosub',
@@ -19,23 +20,25 @@ class Frame(wx.Frame):
         else:
             self.mpc = mpc.MplayerCtrl(self.panel, -1, '../bin/win32/mplayer2.exe',mplayer_args=mplayer_args)
 
+        mainSizer = wx.BoxSizer(wx.VERTICAL)
+        mainSizer.Add(self.mpc, 1, wx.ALL | wx.EXPAND, 5)
+        self.panel.SetSizerAndFit(mainSizer)
 
         self.panel.Bind(mpc.EVT_MEDIA_STARTED, self.media_started)
         self.panel.Bind(mpc.EVT_MEDIA_FINISHED, self.media_finished)
         self.panel.Bind(mpc.EVT_PROCESS_STARTED, self.process_started)
         self.panel.Bind(mpc.EVT_PROCESS_STOPPED, self.process_stopped)
         self.panel.Bind(mpc.EVT_STDERR, self.stderr)
-        self.Bind(wx.EVT_INIT_DIALOG, self.init_dialog)
 
-        self.files = files
+        self.Bind(wx.EVT_CLOSE, self.on_close_window)
 
         self.panel.Layout()
         self.Center()
         self.Show()
 
-    def init_dialog(self, evt):
-        for f in self.files:
-            self.mpc.Loadfile(f, 1) # add to mplayers playlist
+    def on_close_window(self, event):
+        self.mpc.Destroy()
+        self.Destroy()
 
     def media_started(self, evt):
         print '----------> Media started'
@@ -46,6 +49,10 @@ class Frame(wx.Frame):
         print '----------> Media finished'
     def process_started(self, evt):
         print '----------> Process started'
+        for f in self.files:
+            print f
+            self.mpc.Loadfile(unicode(f)) # add to mplayers playlist
+
     def process_stopped(self, evt):
         print '----------> Process stopped'
     def stderr(self, evt):
@@ -56,7 +63,7 @@ if __name__ == '__main__':
     mpc.DEBUG = True
     app = wx.PySimpleApp()
     if sys.platform == 'darwin':
-        f = Frame(None, -1, ['ffmpeg:///Volumes/Untitled 1/Shared/Video/Да Винчи/Demoni.Da.Vinchi.S01E01.L1.HDTVRip.Kerob.avi'])
+        f = Frame(None, -1, [u'ffmpeg:///Volumes/Untitled 1/Shared/Video/Да Винчи/Demoni.Da.Vinchi.S01E01.L1.HDTVRip.Kerob.avi'])
     else:
-        f = Frame(None, -1, ['ffmpeg:///D:/Shared/Video/Да Винчи/Demoni.Da.Vinchi.S01E01.L1.HDTVRip.Kerob.avi'])
+        f = Frame(None, -1, [u'ffmpeg://D:\\\\Shared\\\\Video\\\\Да Винчи\\\\Demoni.Da.Vinchi.S01E01.L1.HDTVRip.Kerob.avi'])
     app.MainLoop()
